c87f9a3c874e6160bb382e9e1fc4c006cd17edc6
Gabriel Lewis <Gabriel-Lewis@users.noreply.github.com>
2016-10-15 12:04:27 -0700

    String Methods

---
+++ strings.js
@@ -0,0 +1,18 @@
+
+
+function isSubstring(searchString, subString) {
+
+}
+
+console.log(isSubstring("time to program", "time")); // true
+console.log(isSubstring("Jump for joy", "joys")); // false
+
+// Write a function that takes three strings - a verb, an adjective, and a
+// noun - uppercases and interpolates them into the sentence
+// "We shall VERB the ADJECTIVE NOUN".
+
+function madLib() {
+
+}
+
+console.log(madLib('make', 'best', 'guac')); // "We shall MAKE the BEST GUAC."

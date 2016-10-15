e6cd7f1bdc582dfd425edcae2539f359893fee44
Gabriel Lewis <Gabriel-Lewis@users.noreply.github.com>
2016-10-15 12:12:52 -0700

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,41 @@
+
+// fizzBuzz
+
+//  Takes an array returns a new array of every number in the array
+//  that is divisible by either 3 or 5, but not both.
+function fizzBuzz(array) {
+
+}
+
+
+function isPrime(number) {
+
+
+}
+
+console.log(isPrime(2)); // true
+console.log(isPrime(10)); // false
+console.log(isPrime(15485863)); // true
+console.log(isPrime(3548563)); // false
+
+
+function firstNPrimes(n) {
+
+}
+
+function sumOfNPrimes(n) {
+
+}
+
+console.log(sumOfNPrimes(0)); // 0
+console.log( sumOfNPrimes(1)); // 2
+console.log(sumOfNPrimes(4)); // 17
+
+// return true if numbers are all divisible by mod
+
+function allOrNothing(mod, ...numbers) {
+
+}
+
+console.log(allOrNothing(3, 9, 12, 6)); // true
+console.log(allOrNothing(5, 1, 2, 10)); // false

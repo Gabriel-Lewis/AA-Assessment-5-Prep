06b2d5739d30dbb0a2f581bf6acc1f79168c3d7b
Gabriel Lewis <Gabriel-Lewis@users.noreply.github.com>
2016-10-15 11:54:02 -0700

    add two methods to arrays

--- arrays.js
+++ arrays.js
@@ -1,32 +1,54 @@
 // Create an unique method for arrays
 //
+
 function myUniq(arr) {
 
 }
+console.log(myUniq([1, 1, 2, 2, 3, 3, 4, 4, 5, 5]));
+
+// Write a unique method for arrays directly on the array class
+//
+
+Array.prototype.myUnique = function () {
+
+}
+
+console.log([1, 1, 2, 2, 3, 3, 4, 4, 5, 5].myUnique());
+
 
 // Write a new twoSum(Array) method that finds all pairs of
 // positions where the elements at those positions sum to zero.
 //
-// twoSum([-1, 0, 2, -2, 1]) # => [[0, 4], [2, 3]]
 //
 
 function twoSum() {
 
 }
 
+console.log(twoSum([-1, 0, 2, -2, 1])); // => [[0, 4], [2, 3]]
+
+// Write a Two sum
+
+Array.prototype.twoSum = function () {
+
+}
+
+console.log([-1, 0, 2, -2, 1].twoSum());
+
 // Write a method, my_transpose, which will convert between the
 // row-oriented and column-oriented representations.
 //
-//        my_transpose([
-//            [0, 1, 2],
-//            [3, 4, 5],
-//            [6, 7, 8]
-//          ])
-//         # => [[0, 3, 6],
-//         #    [1, 4, 7],
-//         #    [2, 5, 8]]
 //
 
 function myTranspose() {
 
 }
+
+console.log(my_transpose([
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8]
+ ]));
+// => [[0, 3, 6],
+//    [1, 4, 7],
+//    [2, 5, 8]]

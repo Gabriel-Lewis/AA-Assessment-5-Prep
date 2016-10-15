fbab98b607a64629177a9eb077292139ea119f5e
Gabriel Lewis <Gabriel-Lewis@users.noreply.github.com>
2016-10-15 15:24:08 -0700

    fixed typos and added test cases to all examples

--- iterations.js
+++ iterations.js
@@ -1,9 +1,20 @@
 // Iterations
 
-function bubbleSort() {
+function bubbleSort(arr) {
 
 }
 
-function substrings() {
+console.log(bubbleSort([192,28,43,29,1,2,200,4])); // [1,2,4,28,29,43,192,200]
+
+Array.prototype.bubbleSort = function () {
+
+};
+
+console.log([192,28,43,29,1,2,200,4].bubbleSort()); // [1,2,4,28,29,43,192,200]
+
+
+function substrings(string) {
 
 }
+
+console.log(substrings("hello")); // ['h','he','hel','hell','hello','e','el','ell','ello','l','ll','llo','l','lo','o']

7e08c9ebbdce3cb64d3ed9b5ef33b21e25a7146a
Gabriel Lewis <Gabriel-Lewis@users.noreply.github.com>
2016-10-19 20:33:45 -0700

    changes Enumerables to include both types

--- enumberables.js
+++ enumberables.js
@@ -1,15 +1,27 @@
 
-function myEach() {
+function myEach(arr, cb) {
 
 }
+
+Array.prototype.myEach = function(cb) => {
+
+};
 
 function myMap() {
 
 }
 
+Array.prototype.myMap = function(cb) {
+
+};
+
 // As the exercise describes, start the accumulator variable with the
 // first value. Iterate through the rest.
 
 function myInject() {
+
+}
+
+Array.prototype.myInject = function() {
 
 }
